cfe987268f05bef8427b232132bb8d836f51c8c0
Erick Villeda <eeverickeev@gmail.com>
2020-07-22 20:36:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 var name = "Joe"
 var height = "74"
 
-if (index.name === "Susan")
+if (index.name === "Susan") {console.log "Name is correct."}
+else {cosole.log "Name is not correct."}
 
 // Don't worry about this
 module.exports = { name, height
